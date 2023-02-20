1660ceea71d35c66dbb1b3a7c60bcfca5c20c0e0
Juan <juanc.saenz@hotmail.com>
2023-02-20 13:45:10 +0100

    Show list dinamically

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,27 +14,13 @@ const Expenses = ({ data }) => {
 
   return (
     <Card className="expenses">
-      <ExpensesFilter onChangeYear={changeYearHandler} yearSelected={filteredYear} />
-      <ExpenseItem
-        title={data[0].title}
-        amount={data[0].amount}
-        date={data[0].date}
+      <ExpensesFilter
+        onChangeYear={changeYearHandler}
+        yearSelected={filteredYear}
       />
-      <ExpenseItem
-        title={data[1].title}
-        amount={data[1].amount}
-        date={data[1].date}
-      />
-      <ExpenseItem
-        title={data[2].title}
-        amount={data[2].amount}
-        date={data[2].date}
-      />
-      <ExpenseItem
-        title={data[3].title}
-        amount={data[3].amount}
-        date={data[3].date}
-      />
+      {data.map((item) => (
+        <ExpenseItem title={item.title} amount={item.amount} date={item.date} />
+      ))}
     </Card>
   );
 };
